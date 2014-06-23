43ef46145b100cdae5170db2280bccd0d09dcd7c
Zach Huenink <zhuenink@gmail.com>
2014-06-22 22:47:16 -0400

    Getting closer to reading in some song files.

--- Fixer.py
+++ Fixer.py
@@ -1,12 +1,25 @@
 #Created by Zach Huenink
-
+import os.path as os
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import EasyMP3 as MP3
 
-def getSongs():
+#Returns a list of songs
+#def getSongs(location):
 	
-
+#Main method
 def fixerMain():
-	getSongs()
+
+	filePath = os.join(input("Please enter the location of your songs. "))
+
+	#try:
+	with open(filePath) as songFolder:
+		songs = songFolder.read()
+		print(songs)
+	songFolder.close()
+	#except IOError:
+	#	print("Invalid file path!")
+
+	print("----------------------\n" + filePath)
+	#getSongs()
 
 fixerMain()
